568932630a87a9c94c7ebd272aa59e22e47c022b
denizerol <deniz.erol@stud-provadis-hochschule.de>
2024-09-16 10:55:12 +0200

    add monitoring in FileService

--- src/main/java/wab/ad/filemanager/FileService.java
+++ src/main/java/wab/ad/filemanager/FileService.java
@@ -1,5 +1,7 @@
 package wab.ad.filemanager;
 
+import io.micrometer.core.instrument.MeterRegistry;
+import io.micrometer.core.instrument.Timer;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
@@ -8,21 +10,51 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.Objects;
 import java.util.UUID;
 
 @Service
 public class FileService {
 
     private final Path rootLocation = Paths.get("upload-dir");
+    private final MeterRegistry meterRegistry;
+
+    @Autowired
+    public FileService(MeterRegistry meterRegistry) {
+        this.meterRegistry = meterRegistry;
+    }
+
 
     public void store(MultipartFile file) throws IOException {
-        String uniqueFilename = UUID.randomUUID().toString() + "_" + file.getOriginalFilename();
-        Files.copy(file.getInputStream(), this.rootLocation.resolve(uniqueFilename));
+        Timer.Sample sample = Timer.start(meterRegistry);
+        try {
+
+            String uniqueFilename = UUID.randomUUID().toString() + "_" + file.getOriginalFilename();
+
+
+            Files.copy(file.getInputStream(), this.rootLocation.resolve(uniqueFilename));
+
+
+            meterRegistry.counter("fileManager_upload_bytes", "fileName", uniqueFilename)
+                    .increment(file.getSize());
+        } finally {
+
+            sample.stop(meterRegistry.timer("fileManager_upload_timer", "fileSize", String.valueOf(file.getSize())));
+        }
     }
 
     public byte[] loadFile(String filename) throws IOException {
-        Path file = rootLocation.resolve(filename);
-        return Files.readAllBytes(file);
+        Timer.Sample sample = Timer.start(meterRegistry);
+        try {
+            Path file = rootLocation.resolve(filename);
+            byte[] fileContent = Files.readAllBytes(file);
+
+            meterRegistry.counter("fileManager_download_bytes", "fileName", filename)
+                    .increment(fileContent.length);
+
+            return fileContent;
+        } finally {
+
+            sample.stop(meterRegistry.timer("fileManager_download_timer", "fileName", filename));
+        }
     }
 }
