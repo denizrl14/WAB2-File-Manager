35b17f7f9305927d8b3a397060c18bf71fe21599
denizerol <deniz.erol@stud-provadis-hochschule.de>
2024-09-16 23:20:43 +0200

    minor changes

--- src/main/java/wab/ad/filemanager/MetricSaver.java
+++ src/main/java/wab/ad/filemanager/MetricSaver.java
@@ -8,19 +8,19 @@ import java.nio.file.Paths;
 
 public class MetricSaver {
 
-    public void saveMetricsToCSV(String timestamp, long fileSize, double uploadTime, double processCpuLoad, long processUsedMemoryString, String identifier) {
+    public void saveMetricsToCSV(String timestamp, long fileSize, double processTime, double processCpuLoad, long processUsedMemoryString, String operation) {
         String csvFile = "metrics.csv";  // Pfad zur CSV-Datei
         boolean fileExists = Files.exists(Paths.get(csvFile));  // Überprüfen, ob die Datei existiert
 
         try (BufferedWriter writer = new BufferedWriter(new FileWriter(csvFile, true))) {
             // Wenn die Datei noch nicht existiert, Kopfzeile hinzufügen
             if (!fileExists) {
-                writer.write("Timestamp,FileSize(Bytes),UploadTime(ms),ProcessCpuLoad(%),ProcessUsedMemory(Bytes),Operation");
+                writer.write("Timestamp,FileSize,ProcessTime,ProcessCpuLoad,ProcessUsedMemory,Operation");
                 writer.newLine();
             }
 
             // CSV-Daten schreiben (Zeitstempel, Dateigröße, Uploadzeit)
-            writer.write(String.format("%s,%d,%.2f,%.2f,%d,%s", timestamp, fileSize, uploadTime, processCpuLoad, processUsedMemoryString, identifier));
+            writer.write(String.format("%s,%d,%.2f,%.2f,%d,%s", timestamp, fileSize, processTime, processCpuLoad, processUsedMemoryString, operation));
             writer.newLine();
         } catch (IOException e) {
             e.printStackTrace();
